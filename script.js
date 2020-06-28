1720c9f46d674e3fe9e617eefda061b904d3ee2d
Ty Hunt <jtmhunt22@gmail.com>
2020-06-28 16:53:07 -0600

    cleaned the code up a little and added comments

--- script.js
+++ script.js
@@ -4,10 +4,9 @@ var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz".split("")
 var numbers = "0123456789".split("")
 var specialCharacters = "`~-=[],./!@#$%^&*()_+{}|:<>?".split("")
 
-var possiblePass = "";
-
-var possibleChar = [];
-var guarChar = [];
+// ! empty variables used later in the project
+var possibleChar = [];//used to push all of the characters into one array
+var possiblePass = ""; //used to create the final password
 
 
 // ! gets html button to manipulate it 
@@ -18,80 +17,60 @@ function getRandom(array) {
   var randomNumber = Math.floor(Math.random() * array.length)
   var randomChar = array[randomNumber]
   return randomChar
-} 
+}
 
-function generatePassword(){
+// ! function used to call all the prompts and return a password
+function generatePassword() {
+  // !prompt the user to enter how many characters they want their password to be
   var userInput = prompt("Between 8-128 how many characters would you like your password to be?")
-if(userInput > 8 && userInput < 128){
-var lowerCase = confirm("Would you like to have lowercase letters")
-var upperCase = confirm("Would you like to have uppercase letters")
-var nums = confirm("Would you like to have numbers letters")
-var characters = confirm("Would you like to have special characters letters")
+  // if statement to have the user enters a number between 8 and 128
+  if (userInput >= 8 && userInput <= 128) {
+    // !lots of confirms to see what the user wants as their password
+    var lowerCase = confirm("Would you like to have lowercase letters")
+    var upperCase = confirm("Would you like to have uppercase letters")
+    var nums = confirm("Would you like to have numbers letters")
+    var characters = confirm("Would you like to have special characters letters")
 
-if (upperCase === true){
-  possibleChar = possibleChar.concat(upperCaseLetters)
+    //confirms to make the user would like the different characters as their password
+    if (upperCase === true) {
+      possibleChar = possibleChar.concat(upperCaseLetters) //adds all of the uppercase letters to the end of the possibleChar array
 
-}
-if (lowerCase === true){
-  possibleChar = possibleChar.concat(lowerCaseLetters)
- 
-}
-if(nums === true){
-  possibleChar = possibleChar.concat(numbers)
+    }
+    if (lowerCase === true) {
+      possibleChar = possibleChar.concat(lowerCaseLetters) //adds all of the lowercase letters to the end of the possibleChar array
 
-}
-if(characters === true){
-  possibleChar = possibleChar.concat(specialCharacters)
-  
-}
-if (characters === false && num === false && lowerCase === false && upperCase === false){
-  alert("You need to press one as true")
-}
-}
-else if(userInput < 8){
-  alert("You need to have more characters")
-}
-else{
-  alert("you need to have less characters")
-}
+    }
+    if (nums === true) {
+      possibleChar = possibleChar.concat(numbers) //adds all of the numbers to the end of the possibleChar array
+    }
+    if (characters === true) {
+      possibleChar = possibleChar.concat(specialCharacters) //adds all of the special characters to the end of the possibleChar array
 
-
-for(var i=0; i<userInput; i++){
-  possiblePass = possiblePass + getRandom(possibleChar);
-}
+    } //if all of the confirms are false then it tells you need to enter at least 1
+    if (characters === false && nums === false && lowerCase === false && upperCase === false) {
+      alert("You need to press one as true")
+    }
+  } //if the user has less than 8 characters it needs to have more characters
+  else if (userInput < 8) {
+    alert("You need to have more characters")
+  } //if the user has more than 128 characters needs to have less
+  else {
+    alert("you need to have less characters")
+  }
+  //loops through the users input of characters
+  for (var i = 0; i < userInput; i++) {
+    possiblePass = possiblePass + getRandom(possibleChar); //adds a single character to the password from the array possiblePass each time the loop condition is true 
+  }
 }
 
+// !function to write the password to the screen
 function writePassword() {
-  generatePassword();
-  var password = document.querySelector("#password")
-  password.textContent = possiblePass;
+  generatePassword(); //calls the generate password function
+  var password = document.querySelector("#password") // ! calls the id password to write the password to the screen
+  password.textContent = possiblePass; // ! gets the possiblePass variable and writes it to the screen
 
 }
 
 
-// Add event listener to generate button
+// ! Add event listener to generate button so when the button is pressed writePassword function will be applied
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
